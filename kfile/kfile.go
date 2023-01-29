5473a038567d371fd93d7091b085588c7fd09b38
kouler <koulerz@gmail.com>
2023-01-29 23:54:34 +0800

    feat: 完善文件相关函数

--- kfile/kfile.go
+++ kfile/kfile.go
@@ -1,6 +1,7 @@
 package kfile
 
 import (
+	"bytes"
 	"os"
 )
 
@@ -47,4 +48,41 @@ func IsFile(path string) bool {
 		return !fi.IsDir()
 	}
 	return false
+}
+
+// ReadFile 读文件
+func ReadFile(name string) ([]byte, error) {
+	return os.ReadFile(name)
+}
+
+// ReadLines 按行读取文件
+func ReadLines(name string) ([][]byte, error) {
+	cont, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.Split(cont, []byte("\n")), nil
+}
+
+// WriteFile 向文件写入内容
+// 若文件不存在，将以 0666 权限创建新文件
+func WriteFile(name string, data []byte) error {
+	return os.WriteFile(name, data, 0666)
+}
+
+// AppendFile 向文件追加内容
+// 若文件不存在，将以 0666 权限创建新文件
+func AppendFile(name string, data []byte) error {
+	f, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	if _, err := f.Write(data); err != nil {
+		f.Close()
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+	return nil
 }
