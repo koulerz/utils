0d3eee8f8a77ba0434d86c48779d685f670f6e89
kouler <koulerz@gmail.com>
2023-01-29 21:16:31 +0800

    feat: 完善文件操作

--- kfile/kfile.go
+++ kfile/kfile.go
@@ -1,33 +1,50 @@
 package kfile
 
-import "os"
+import (
+	"os"
+)
 
 // IsPathExist 判断路径是否存在
-func IsPathExist(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
-
-// IsFileExist 判断文件是否存在
-func IsFileExist(path string) bool {
-	s, err := os.Stat(path)
-	if err != nil && os.IsNotExist(err) {
+func IsPathExist(path string) bool {
+	if path == "" {
 		return false
 	}
-	return !s.IsDir()
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsFileExists 判断文件是否存在
+func IsFileExists(path string) bool {
+	return IsFile(path)
 }
 
 // IsDirExist 判断目录是否存在
 func IsDirExist(path string) bool {
-	s, err := os.Stat(path)
-	if err != nil && os.IsNotExist(err) {
+	return IsDir(path)
+}
+
+// IsDir 判断是否为目录
+func IsDir(path string) bool {
+	if path == "" {
 		return false
 	}
-	return s.IsDir()
+	if fi, err := os.Stat(path); err == nil {
+		return fi.IsDir()
+	}
+	return false
+}
+
+// IsFile 判断是否为文件
+func IsFile(path string) bool {
+	if path == "" {
+		return false
+	}
+	if fi, err := os.Stat(path); err == nil {
+		return !fi.IsDir()
+	}
+	return false
 }
